a740b789fb9b6c5e2df42be4797a61e8be39ceb5
Dru1DD <mrtproger@gmail.com>
2024-09-07 14:53:53 +0200

    add optimism chain

--- src/lib/wagmi/config.ts
+++ src/lib/wagmi/config.ts
@@ -1,10 +1,12 @@
 import { http, createConfig } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, optimism,, optimismSepolia } from "wagmi/chains";
 
 export const wagmiConfig = createConfig({
   chains: [mainnet, sepolia],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [optimism.id]: http(),
+    [optimismSepolia.id]: http()
   },
 });
