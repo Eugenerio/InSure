3cdb742a2c7c5fc09937bc10d9ca4a0d99dbf2d9
Eugenerio <res_2005@ukr.net>
2024-09-07 23:45:59 +0200

    feat: integrate admin side contract content

--- src/components/molecules/admin-modal.tsx
+++ src/components/molecules/admin-modal.tsx
@@ -7,6 +7,11 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button, Label, Input } from "@/components";
+import { toast } from "react-toastify";
+import { writeContract, getTransactionReceipt } from "@wagmi/core";
+import { wagmiConfig } from "@/lib/wagmi/config";
+import { erc20Abi, parseUnits } from "viem";
+import { insuranceAbi, insuranceAddress } from "@/lib/wagmi/contract/abi";
 
 export const AdminModal = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
@@ -14,20 +19,165 @@ export const AdminModal = () => {
   const [withdrawLiquidityAmount, setWithdrawLiquidityAmount] =
     useState<string>("");
 
-  const handleAddLiquidity = () => {
-    if (addLiquidityAmount) {
-      console.log(`Adding liquidity: ${Number(addLiquidityAmount)}`);
+  const approve = async (amount: bigint) => {
+    try {
+      const txResponse = await writeContract(wagmiConfig, {
+        address: "0x49d2c1D548ae5767D66DF42f74c3eab4f35490e1",
+        abi: erc20Abi,
+        functionName: "approve",
+        args: [insuranceAddress, amount],
+      });
+
+      let receipt;
+      const pollInterval = 1000;
+      const maxRetries = 30;
+      for (let i = 0; i < maxRetries; i++) {
+        try {
+          receipt = await getTransactionReceipt(wagmiConfig, {
+            hash: txResponse,
+          });
+          if (receipt) {
+            break;
+          }
+        } catch (e) {}
+        await new Promise((resolve) => setTimeout(resolve, pollInterval));
+      }
+
+      if (receipt && receipt.status === "success") {
+        toast.success("Approved!");
+      } else {
+        toast.error("Approval failed");
+      }
+    } catch (err: any) {
+      toast.error("Approval failed: " + err.message);
     }
   };
 
-  const handleWithdrawLiquidity = () => {
+  const add_liquidity = async (amount: bigint) => {
+    try {
+      const txResponse = await writeContract(wagmiConfig, {
+        address: insuranceAddress,
+        abi: insuranceAbi,
+        functionName: "addLiquidity",
+        args: [amount],
+      });
+
+      let receipt;
+      const pollInterval = 1000;
+      const maxRetries = 30;
+      for (let i = 0; i < maxRetries; i++) {
+        try {
+          receipt = await getTransactionReceipt(wagmiConfig, {
+            hash: txResponse,
+          });
+          if (receipt) {
+            break;
+          }
+        } catch (e) {}
+        await new Promise((resolve) => setTimeout(resolve, pollInterval));
+      }
+
+      // Check receipt status
+      if (receipt && receipt.status === "success") {
+        toast.success("Liquidity added successfully!");
+      } else {
+        toast.error("Failed to add liquidity");
+      }
+    } catch (err: any) {
+      toast.error("An error occurred: " + err.message);
+    }
+  };
+
+  const widthdraw_liquidity = async (amount: bigint) => {
+    try {
+      const txResponse = await writeContract(wagmiConfig, {
+        address: insuranceAddress,
+        abi: insuranceAbi,
+        functionName: "widthdrawLiquidity",
+        args: [amount],
+      });
+
+      let receipt;
+      const pollInterval = 1000;
+      const maxRetries = 30;
+      for (let i = 0; i < maxRetries; i++) {
+        try {
+          receipt = await getTransactionReceipt(wagmiConfig, {
+            hash: txResponse,
+          });
+
+          if (receipt) {
+            break;
+          }
+        } catch (e) {}
+        await new Promise((resolve) => setTimeout(resolve, pollInterval));
+      }
+
+      if (receipt && receipt.status === "success") {
+        toast.success("Liquidity withdrawed successfully!");
+      } else {
+        toast.error("Failed to withdrawed liquidity");
+      }
+    } catch (err: any) {
+      toast.error("An error occurred: " + err.message);
+    }
+  };
+
+  const collect_fee = async () => {
+    try {
+      const txResponse = await writeContract(wagmiConfig, {
+        address: insuranceAddress,
+        abi: insuranceAbi,
+        functionName: "collectFee",
+      });
+
+      let receipt;
+      const pollInterval = 1000;
+      const maxRetries = 30;
+      for (let i = 0; i < maxRetries; i++) {
+        try {
+          receipt = await getTransactionReceipt(wagmiConfig, {
+            hash: txResponse,
+          });
+
+          if (receipt) {
+            break;
+          }
+        } catch (e) {}
+        await new Promise((resolve) => setTimeout(resolve, pollInterval));
+      }
+
+      if (receipt && receipt.status === "success") {
+        toast.success("Fee collected successfully!");
+      } else {
+        toast.error("Failed to collect fees liquidity");
+      }
+    } catch (err: any) {
+      toast.error("An error occurred: " + err.message);
+    }
+  };
+
+  const handleAddLiquidity = async () => {
+    if (addLiquidityAmount) {
+      const amountInBigInt = parseUnits(addLiquidityAmount, 18);
+      console.log(addLiquidityAmount);
+      await approve(amountInBigInt);
+      console.log(`Adding liquidity: ${Number(addLiquidityAmount)}`);
+      await add_liquidity(amountInBigInt);
+    }
+  };
+
+  const handleWithdrawLiquidity = async () => {
     if (withdrawLiquidityAmount) {
       console.log(`Withdrawing liquidity: ${Number(withdrawLiquidityAmount)}`);
+      const amountInBigInt = parseUnits(addLiquidityAmount, 18);
+      await widthdraw_liquidity(amountInBigInt);
     }
   };
 
-  const handleCollectFee = () => {
+  const handleCollectFee = async () => {
     console.log("Collecting fee");
+    await collect_fee();
   };
 
   const closeHandler = () => {
