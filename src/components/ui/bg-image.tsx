c37ece112ca30d8aeb9b88aa58b567da3f0c8061
Dru1DD <mrtproger@gmail.com>
2024-09-07 17:37:28 +0200

    update: bg image size

--- src/components/ui/bg-image.tsx
+++ src/components/ui/bg-image.tsx
@@ -1,13 +1,14 @@
 import Image from "next/legacy/image";
 
 export const BGImage = () => (
-  <div className={"absolute -z-10 w-full h-screen"}>
-    <div className={"absolute top-0 left-0 w-full h-full blur-xl z-0"}>
+  <div className={"absolute -z-10 w-full h-full"}>
+    <div className={"absolute top-0 left-0 w-full h-full  z-0"}>
       <Image
         src={"/1.jpeg"}
         layout="fill"
         quality={100}
-        objectFit={"cover blur-xl"}
+        objectFit={"cover"}
+        className={"blur-xl"}
         alt={"bg"}
       />
     </div>
