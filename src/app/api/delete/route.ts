7c560317989e4209d4f6e1b0cc07e335023eacca
Eugenerio <res_2005@ukr.net>
2024-09-08 02:33:47 +0200

    feat: add delete

---
+++ src/app/api/delete/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req: Request) {
+  try {
+    const data = await req.json();
+    const { policyId } = data;
+
+    await prisma.insurence.delete({
+      where: {
+        policyId,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      objects: [],
+    });
+  } catch (e: any) {
+    console.log(e);
+    return NextResponse.json({
+      message: e.message,
+    });
+  }
+}
+export async function GET() {}
